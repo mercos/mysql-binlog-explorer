d449a2f9a578ab1d5c639ae2460b68e0106c5521
Jorge Klemm <jorge@meuspedidos.com.br>
2018-01-31 14:53:01 -0200

    Created Json response

--- binloganalyser/web.py
+++ binloganalyser/web.py
@@ -13,9 +13,30 @@ def index():
 
 @route('/binlog-parser/')
 def binlog_parser():
-    return {
-        "data": [simplejson.dumps(transactions, ensure_ascii=False, for_json=True)]
+    return binlog_parser_presenter(transactions)
+
+
+def binlog_parser_presenter(list_of_transactions):
+    response = {
+        'data': [{
+            'start_date': transaction.start_date.strftime("%Y-%m-%d %H:%M:%S") if transaction.start_date else '',
+            'end_date': transaction.end_date.strftime("%Y-%m-%d %H:%M:%S") if transaction.end_date else '',
+            'duration': transaction.duration,
+            'total_changes': transaction.total_changes,
+            'statements': [{
+                'changes': [{
+                    'actual_command': change.actual_command,
+                    'command_type': change.command_type
+                } for change in statement.changes]
+            } for statement in transaction.statements]
+        } for transaction in list_of_transactions]
     }
+    
+    return json.dumps(response, ensure_ascii=False)
 
+if __name__ == '__main__':
+    bottle.debug(True)
 
-run(host='localhost', port=8080)
+    transactions = BinlogParser().parse(open('tests/examples/binarylog_01_31_1145.log'))
+
+    run(host='localhost', port=8080)
---
+++ tests/test_web.py
@@ -0,0 +1,31 @@
+from unittest.case import TestCase
+
+from datetime import datetime
+
+from binloganalyser.binlog_parser import Transaction, Statement, Change
+from binloganalyser.web import binlog_parser_presenter
+
+
+class WebTests(TestCase):
+    def test_convert_list_of_transaction_to_dict(self):
+        transactions = [
+            Transaction(
+                datetime(2018, 1, 1, 12, 1, 1),
+                datetime(2018, 1, 1, 12, 1, 2),
+                [
+                    Statement([
+                        Change('UPDATE', 'UPDATE SHU SET X = 1')
+                    ])
+                ]
+            )
+        ]
+
+        result = binlog_parser_presenter(transactions)
+        
+        self.assertEqual(1, len(result['data']))
+        self.assertEqual(datetime(2018, 1, 1, 12, 1, 1), result['data'][0]['start_date'])
+        self.assertEqual(datetime(2018, 1, 1, 12, 1, 2), result['data'][0]['end_date'])
+        self.assertEqual(1, len(result['data'][0]['statements']))
+        self.assertEqual(1, len(result['data'][0]['statements'][0]['changes']))
+        self.assertEqual('UPDATE', result['data'][0]['statements'][0]['changes'][0]['command_type'])
+        self.assertEqual('UPDATE SHU SET X = 1', result['data'][0]['statements'][0]['changes'][0]['actual_command'])
