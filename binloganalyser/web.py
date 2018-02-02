2720e605bcfadc1ac75e7fce633ea2b8804ac0fd
Israel Fonseca <israel@meuspedidos.com.br>
2018-02-02 13:37:52 -0200

    You may pass multiple files to it.

--- binloganalyser/web.py
+++ binloganalyser/web.py
@@ -1,7 +1,7 @@
 import json
+import sys
 
 import bottle
-import sys
 from bottle import route, run, template
 
 from binlog_parser import BinlogParser
@@ -37,12 +37,17 @@ def binlog_parser_presenter(list_of_transactions):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: mysql-binlog-explorer <binlog.file>")
+    if len(sys.argv) <= 1:
+        print("Usage: mysql-binlog-explorer <binlog.file1> <binlog.file2> <binlog.file...N>")
         exit(1)
 
     bottle.debug(True)
+    files = sys.argv[1:]
+    transactions = []
 
-    transactions = BinlogParser().parse(open(sys.argv[1]))
+    for binlog_file in files:
+        print('Parsing {}'.format(binlog_file))
+        with open(binlog_file) as content:
+            transactions += BinlogParser().parse(content)
 
     run(host='localhost', port=8080)
