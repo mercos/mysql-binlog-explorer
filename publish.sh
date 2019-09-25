01c4d887e7e7c889afcb815cef0ddda2a23ed4d0
Caio Fernando Bertoldi Paes de Andrade <caiofbpa@icloud.com>
2019-09-25 09:19:17 -0300

    Update pypi address

--- publish.sh
+++ publish.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-version=$(cat binlogexplorer/version)
-statusCode=$(curl --write-out %{http_code} --silent --output /dev/null https://pypi.python.org/pypi/mysql-binlog-explorer/${version})
+VERSION=$(cat binlogexplorer/version)
 
-if [[ ${statusCode} = "200" ]]; then
-  echo "Version '${version}' already published. Did you forget to update the version code?"
+STATUS_CODE=$(curl --write-out %{http_code} --silent --output /dev/null https://pypi.org/project/mysql-binlog-explorer/$V{ERSION}/)
+
+https://pypi.org/project/mysql-binlog-explorer/0.2.3/
+if [[ ${STATUS_CODE } = "200" ]]; then
+  echo "Version '${VERSION}' already published. Did you forget to update the version code?"
   exit 1
 fi
 
@@ -13,9 +15,9 @@ echo 'Preparing dist dir...'
 python setup.py sdist
 
 echo 'Testing distribution and installation...'
-twine upload --repository-url https://test.pypi.org/legacy/ dist/mysql-binlog-explorer-${version}.tar.gz --skip-existing -u ${TEST_PYPI_USER} -p ${TEST_PYPI_PASSWORD}
+twine upload --repository-url https://test.pypi.org/legacy/ dist/mysql-binlog-explorer-${VERSION}.tar.gz --skip-existing -u ${TEST_PYPI_USER} -p ${TEST_PYPI_PASSWORD}
 pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple mysql-binlog-explorer
 mysql-binlog-explorer -h
 
 echo 'Publishing on PyPi'
-twine upload dist/mysql-binlog-explorer-${version}.tar.gz -u ${PYPI_USER} -p ${PYPI_PASSWORD}
+twine upload dist/mysql-binlog-explorer-${VERSION}.tar.gz -u ${PYPI_USER} -p ${PYPI_PASSWORD}
