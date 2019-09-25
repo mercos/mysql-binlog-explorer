01f2dadd0d4e18ad5f0c662c2fec84ce04391315
Caio Fernando Bertoldi Paes de Andrade <caiofbpa@icloud.com>
2019-09-25 08:31:47 -0300

    Remove more sudos and setup secrets on env

--- publish.sh
+++ publish.sh
@@ -2,8 +2,7 @@
 set -e
 
 version=$(cat binlogexplorer/version)
-statusCode=$(curl --write-out %{http_code} --silent \
-                  --output /dev/null https://pypi.python.org/pypi/mysql-binlog-explorer/${version})
+statusCode=$(curl --write-out %{http_code} --silent --output /dev/null https://pypi.python.org/pypi/mysql-binlog-explorer/${version})
 
 if [[ ${statusCode} = "200" ]]; then
   echo "Version '${version}' already published. Did you forget to update the version code?"
@@ -11,14 +10,12 @@ if [[ ${statusCode} = "200" ]]; then
 fi
 
 echo 'Preparing dist dir...'
-rm -rf dist/*
 python setup.py sdist
 
 echo 'Testing distribution and installation...'
-echo 'odus' | sudo -S pip uninstall mysql-binlog-explorer -y || true
-twine upload --repository-url https://test.pypi.org/legacy/ dist/mysql-binlog-explorer-${version}.tar.gz \
-        --skip-existing -u ${TEST_PYPI_USER} -p ${TEST_PYPI_PASSWORD}
-echo 'odus' | sudo -S pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple mysql-binlog-explorer
+pip uninstall mysql-binlog-explorer -y
+twine upload --repository-url https://test.pypi.org/legacy/ dist/mysql-binlog-explorer-${version}.tar.gz --skip-existing -u ${TEST_PYPI_USER} -p ${TEST_PYPI_PASSWORD}
+pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple mysql-binlog-explorer
 mysql-binlog-explorer -h
 
 echo 'Publishing on PyPi'
